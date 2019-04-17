2da7c09e4112ad9ba61e5dab6cbc68a17c468265
Carrosen <carla1.rosen@gmail.com>
2019-04-17 12:13:01 +0200

    added scrollToTop funtion to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 
+const scrollToTop = () => {
+    // window.scrollTo(0, 0)
+    let mainSection = document.getElementById('main-section')
+    mainSection.scrollTop = 0
+    document.documentElement.scrollTop = 0
+
+}
+
 const Footer = () => {
     return (
         <div className="footer-wrapper">
             <div className="footer-content">
                 Made with React
+                <button onClick={scrollToTop}>Scroll To Top</button>
             </div>
         </div>
     )
