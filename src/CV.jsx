1037b42c936ee8f133ebe75f6f86696538c27870
Carrosen <carla1.rosen@gmail.com>
2019-04-20 16:43:48 +0200

    added CV to cv.json file and styled it

--- src/CV.jsx
+++ src/CV.jsx
@@ -26,7 +26,7 @@ class CV extends Component {
         if (cv.length > 0) {
             cvList = cv.map(cv => {
                 return (
-                    <div key={cv.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                    <div key={cv.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-2/3">
                         <CvCard cv={cv} />
                     </div>
                 )
@@ -35,14 +35,9 @@ class CV extends Component {
 
         return (
             <div className="content-wrapper">
-                <div className="flex mb-4">
-                    <div className="w-3/4">
-                        <h1 className="coCVntent-text">CV</h1>
-                    </div>
 
-                </div>
 
-                <div className="flex flex-wrap -mx-1 lg:-mx-4">
+                <div className="flex flex-wrap justify-center mx-auto">
                     {cvList}
                 </div>
             </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
 
-        <div className="flex flex-wrap justify-center bg-moss-tide">
+        <div className="flex flex-wrap justify-center bg-white">
             <nav className="header">
                 <h1 className="header-title">
                     <Link className="h1-link hover" to='/'>CARLA ROSÉN</Link> 
